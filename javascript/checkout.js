8bb1e8dec33c69d277e8c3b1c9c72f385bdfecfc
VN OFFICIAL <vardhank319@gmail.com>
2024-07-07 01:14:19 +0700

    checkout head finished

---
+++ javascript/checkout.js
@@ -0,0 +1,26 @@
+import { renderCartCount } from "./amazon";
+const html=
+ `
+    <header class="header">
+        <section class="left-header">
+            <a href="./amazon.html">
+                <img src="../images/amazon-mobile-logo.png" class="amazon-mobile-logo">
+            </a>
+            <a href="./amazon.html">
+                <img src="../images/amazon-logo.png" class="amazon-logo">
+            </a>
+        </section>
+        <section class="center-header">
+            <div class="checkout-txt">
+                Checkout (<div class="js-cart-count">${renderCartCount} items</div><div>)</div>
+            </div>
+        </section>
+        <section class="right-header">
+            <img src="../images/icons/checkout-lock-icon.png">
+        </section>
+    </header>
+
+ `;
+
+ document.querySelector('.js-header-div').innerHTML=html;
+ console.log(html);
