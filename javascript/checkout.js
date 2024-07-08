4831f08c3e3b5cc87e7e76e1ec94073760321414
VN OFFICIAL <vardhank319@gmail.com>
2024-07-09 01:17:14 +0700

    checkout started

--- javascript/checkout.js
+++ javascript/checkout.js
@@ -1,24 +1,4 @@
-const html=
- `
-    <header class="header">
-        <section class="left-header">
-            <a href="./amazon.html">
-                <img src="../images/amazon-mobile-logo.png" class="amazon-mobile-logo">
-            </a>
-            <a href="./amazon.html">
-                <img src="../images/amazon-logo.png" class="amazon-logo">
-            </a>
-        </section>
-        <section class="center-header">
-            <div class="checkout-txt">
-                Checkout (<div class="js-cart-count">3 items</div><div>)</div>
-            </div>
-        </section>
-        <section class="right-header">
-            <img src="../images/icons/checkout-lock-icon.png">
-        </section>
-    </header>
+import { renderCheckOutHeader,renderCheckOutBody } from "./html-generators-js/checkout-html.js";
 
- `;
-
- document.querySelector('.js-header-div').innerHTML=html;
+renderCheckOutHeader();
+renderCheckOutBody();
